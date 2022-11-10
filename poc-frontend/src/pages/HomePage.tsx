ce3a56f56db62a56878eadd3933346720897b604
noephiliprestum <noephilip.restum@bicol-u.edu.ph>
2022-11-10 14:36:46 +0800

    fix code smell for modal

--- poc-frontend/src/pages/HomePage.tsx
+++ poc-frontend/src/pages/HomePage.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import Stack from "@mui/material/Stack";
 
 import MovieBanner from "components/MovieBanner";
 import SliderRowForGenre from "components/VideoSlider";
+import { useDetailModal } from "providers/DetailModalProvider";
 
 const HomePage = () => {
+  const { onClose } = useDetailModal();
+
+  useEffect(() => {
+    onClose();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <Stack spacing={2} sx={{ bgcolor: "background.default" }}>
       <MovieBanner />
